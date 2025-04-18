b1d3597ff3b5fdae9de5bc320a69d2a861b2a397
HASINMMPM <hasinmmpm786@gmail.com>
2025-04-18 21:39:14 +0530

    feat(error_Middlware): implemented Custom error class for handling application-specific errors. And Middleware for handling errors in the application.
    ErrorMessage class extends the built-in `Error` class to include additional properties such as statusCode and isOperational for better error handling.  errorMiddleware captures errors thrown during request processing and sends
    
    - implement errorHandler function  it return statuscode and message
    - implement errorMessage class which is extend from Error
    - added errorHandler to server.ts
    
    Closes #INK63 - https://nizzcorp-i.atlassian.net/browse/OBI-63?atlOrigin=eyJpIjoiNWI3ZTRiNzgxYmEwNDYwZWJkNWRhMjE2N2ZmZDJlYzYiLCJwIjoiaiJ9

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import express from "express";
 import { PORT } from "./utils/env";
+import { errorHandler } from "./middlewares/errorMiddleware";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
